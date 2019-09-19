0abe8fe3f54c3bc65c9075eec3ab4e6ff4a4af41
吴创业 <501710163@qq.com>
2019-09-19 18:08:40 +0800

    在model类中创建AppDbContext类 在Startup类中添加userDBContextPool 在appsettings中添加连接字符串

--- WebApplication1/Models/Student.cs
+++ WebApplication1/Models/Student.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         [Required(ErrorMessage ="请输入名字"),Display(Name="名字"),MaxLength(50,ErrorMessage ="名字长度不可以超过50个字符")]
         public string Name { get; set; }
-        [Display(Name= "编号")]
-        public NumberEnum Number { get; set; }
+        [Required(ErrorMessage = "请选择编号"), Display(Name= "编号")]
+        public NumberEnum? Number { get; set; }
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplication1.Models;
 
@@ -13,10 +15,17 @@ namespace WebApplication1
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options=>options.UseSqlServer(_configuration.GetConnectionString("StudentDBConnection")));
             services.AddMvc();
             services.AddSingleton <IStudentRepository,MockStudentRepository> ();
         }
