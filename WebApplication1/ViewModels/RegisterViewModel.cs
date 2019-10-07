b27bc2fc44d3a55f8b8fc5459c876db5fcf05885
吴创业 <501710163@qq.com>
2019-10-07 18:00:18 +0800

    1、新增注册页面，相应控制器和viewmodel 2、母版页新增导航栏右侧注册

---
+++ WebApplication1/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "邮箱地址")]
+        [Required]
+        [EmailAddress]
+        public string  Email { get; set; }
+        [Display(Name = "密码")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="确认密码")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="两次密码不一致，请重新输入")]
+        public string ConfirmPassword { get; set; }
+    }
+}
