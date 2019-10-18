2bb8ca07af1d3d3e43a8676f9fc3016af3bf2436
吴创业 <501710163@qq.com>
2019-10-18 19:06:24 +0800

    创建角色控制器 视图 模型

---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController:Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                //如果创建同名的角色会报错
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index","home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
